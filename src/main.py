b98e657637fe281377638c4e240cb71ce6cd63ed
AsterVAIZ <ivanostapuk200511@gmail.com>
2026-05-15 00:14:49 +0300

    feat: initial project structure

---
+++ src/main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+def greet(name: str) -> srt:
+    """Повертває вітальне повідомлення."""
+    return f"Привет, {name)!"
+
+
+if __name__ == "__main__":
+    print(greet("Студент")) 
